9e4f65b052a4cf1cd634e4c7ddfd035d7cd8e14e
erlie <290059742@qq.com>
2019-04-06 23:47:21 +0800

    项目初始化

--- src/main/java/com/generator/demo/dao/StudentMapper.java
+++ src/main/java/com/generator/demo/dao/StudentMapper.java
@@ -1,30 +1,15 @@
 package com.generator.demo.dao;
 
 import com.generator.demo.entity.Student;
-import com.generator.demo.entity.StudentExample;
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface StudentMapper {
-    long countByExample(StudentExample example);
-
-    int deleteByExample(StudentExample example);
-
     int deleteByPrimaryKey(Integer id);
 
     int insert(Student record);
 
     int insertSelective(Student record);
 
-    List<Student> selectByExample(StudentExample example);
-
     Student selectByPrimaryKey(Integer id);
-
-    int updateByExampleSelective(@Param("record") Student record, @Param("example") StudentExample example);
-
-    int updateByExample(@Param("record") Student record, @Param("example") StudentExample example);
 
     int updateByPrimaryKeySelective(Student record);
 
